61e4291b9b348b925f6fd2df0eefae7557137099
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-04-27 22:55:51 +0100

    edited for mobile, edited the checkbox, cleaner js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const todo = (document.getElementById("addTodo").onclick = () => {
+const inputTodo = document.getElementById("todo");
+const error = document.querySelector(".error");
+const list = document.getElementById("list");
+const todo = document.getElementById("addTodo");
+
+const appendTask = () => {
    const li = document.createElement("li");
    li.classList.add("inp1");
 
@@ -12,77 +17,55 @@ const todo = (document.getElementById("addTodo").onclick = () => {
    const textNode = document.createTextNode(text);
    para.append(textNode);
    li.appendChild(para);
-   document.getElementById("list").append(li);
+   list.append(li);
 
    const image = document.createElement("img");
    image.classList.add("bin");
    image.src = "outline_delete_forever_black_24dp.png";
    li.appendChild(image);
 
-   const button = document.createElement("BUTTON");
-   button.classList.add("bttn");
+   const button = document.createElement("div");
+   button.classList.add("bin-icon");
    button.appendChild(image);
    li.appendChild(button);
+};
 
-   
-   var deletes = document.querySelectorAll(".bttn");
+const iconDelete = () => {
+   var deletes = document.querySelectorAll(".bin-icon");
    deletes.forEach((btn) => {
       btn.addEventListener("click", function () {
-         const lis = this.parentNode;
-         lis.remove();
+         this.parentNode.remove();
       });
    });
-   
-   
-   
-});
+};
 
+const crossTask = () => {
+   var crossTodo = list.querySelectorAll("p");
+   crossTodo.forEach((text) => {
+      text.addEventListener("click", function () {
+         this.classList.toggle("completed");
 
+         // if (text.classList.contains = "completed") {
+         //    console.log('Complete');
+         //    input.style.transform = "scale(1)";
+         // }
+      });
+   });
+};
 
-// var rem = document.getElementById("bin");
+const addTaskHandler = () => {
+   if (inputTodo.value.length == 0) {
+      error.style.display = "flex";
+      inputTodo.style.border = "thick solid #F96464";
+   } else {
+      error.style.display = "none";
+      inputTodo.style.border = "none";
+      appendTask();
+      iconDelete();
+      crossTask();
 
-// for (let i = 0; i < rem.length; i++) {
-//    var myBtn = rem[i];
-// }
+      inputTodo.value = "";
+   }
+};
 
-// rem.addEventListener("click", function () {
-//    const item = this.parentNode.parentNode;
-//    const parent = item.parentNode;
-//    parent.removeChild(item);
-// });
-
-function clear() {
-   document.getElementById('todo').reset()
-}
-
-// const lin = document.querySelector('.inp1')
-
-// const lis = [...document.querySelectorAll(".inp1")];
-// for (const li of lis) {
-//    li.addEventListener("click", handleDelete);
-// }
-
-// function handleDelete() {
-//    const lim = document.getElementById("list");
-//    const lip = document.getElementById('intt')
-//    lim.parentNode.removeChild(lip);
-//    //   this.parentNode.removeChild(this);
-// }
-
-// handleDelete()
-
-// const li = document.querySelector('.inp1')
-// li.addEventListener('click', function () {
-//     console.log(this);
-// })
-
-// document.getElementById("item").value = "";
-
-// function removeTodo() {
-//    btnRem.removeEventListener("click", removeTodo);
-// }
-
-// var a = document.getElementById("bin");
-// var b = document.getElementById("todo");
-// var rem = document.getElementById("todo").value;
-// a.removeChild(rem);
+todo.addEventListener("click", addTaskHandler);
